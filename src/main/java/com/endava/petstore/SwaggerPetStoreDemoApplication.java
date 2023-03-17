c072a6bfa41838b3f96e91f1dbf7793372776717
GeorgeBacalu <georgebacalu83@gmail.com>
2023-03-17 08:23:16 +0200

    Initial commit

---
+++ src/main/java/com/endava/petstore/SwaggerPetStoreDemoApplication.java
@@ -0,0 +1,13 @@
+package com.endava.petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerPetStoreDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerPetStoreDemoApplication.class, args);
+    }
+
+}
